98e3e0fd93b3493f9b38368180bd2a95a4b1f986
Muhammad Faisal <faisalmd25121999@gmail.com>
2025-09-13 19:57:09 +0530

    Time: 3 ms (8.13%) | Memory: 9.2 MB (8.75%) - LeetSync

--- 2361-calculate-digit-sum-of-a-string/calculate-digit-sum-of-a-string.cpp
+++ 2361-calculate-digit-sum-of-a-string/calculate-digit-sum-of-a-string.cpp
@@ -9,11 +9,8 @@ public:
         for(int i = 0;i<s.size();i++){
             int digit = s[i]-'0';
             sum+=digit;
-            cout<<cnt<<" "<<sum<<endl;
-
             cnt++;
             if(cnt == k){
-                cout<<sum<<endl;
                 result += to_string(sum);
                 cnt = 0;
                 sum = 0;
