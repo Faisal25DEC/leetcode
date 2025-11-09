ceae95c96dc0c1897211312bad2ab49d0aa48170
Muhammad Faisal <faisalmd25121999@gmail.com>
2025-11-09 14:57:31 +0530

    Time: 16 ms (5.17%) | Memory: 58.4 MB (21.03%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.ts
@@ -0,0 +1,28 @@
+function longestPalindrome(s: string): number {
+    const freqMap = new Map<string,number>();
+
+    for(const char of s) {
+        const freq = freqMap.get(char)??0;
+        freqMap.set(char,freq+1)
+    }
+
+    let count = 0;
+    let isOdd = false;
+    let maxOdd = 0;
+    freqMap.forEach((value,key)=>{
+        console.log(value)
+        if(value%2 === 1){
+            isOdd = true;
+            count+=value-1;
+        }
+        else{
+            count += value;
+            console.log(count)
+        }
+        
+
+    })
+
+    return count + +(isOdd?1:0)
+
+};
