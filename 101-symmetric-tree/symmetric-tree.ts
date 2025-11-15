a7fe10b58d94a31a3328276273ce944fa9b0e096
Muhammad Faisal <faisalmd25121999@gmail.com>
2025-11-15 12:22:09 +0530

    Time: 29 ms (5.19%) | Memory: 63.6 MB (5.19%) - LeetSync

---
+++ 101-symmetric-tree/symmetric-tree.ts
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+ function isMirror(root1:TreeNode | null , root2:TreeNode | null):boolean{
+    
+    let res = false
+
+    if(root1 && root2){
+         console.log(root1,root2)
+         res =  (root1.val === root2.val) && isMirror(root1.left, root2.right) && isMirror(root1.right, root2.left);
+         return res;
+    }
+    console.log(res)
+    return root1 === root2
+
+    
+ }
+
+function isSymmetric(root: TreeNode | null): boolean {
+        return root === null || (isMirror(root.left, root.right))
+};
