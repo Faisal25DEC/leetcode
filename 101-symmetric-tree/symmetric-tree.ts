eb176074388504ed905a33a8dc34b85a0097efba
Muhammad Faisal <faisalmd25121999@gmail.com>
2025-11-15 12:24:33 +0530

    Time: 0 ms (100.00%) | Memory: 58.2 MB (62.97%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.ts
+++ 101-symmetric-tree/symmetric-tree.ts
@@ -14,11 +14,10 @@
 
  function isMirror(root1:TreeNode | null , root2:TreeNode | null):boolean{
     
-    let res = false
 
     if(root1 && root2){
-         res =  (root1.val === root2.val) && isMirror(root1.left, root2.right) && isMirror(root1.right, root2.left);
-         return res;
+         return (root1.val === root2.val) && isMirror(root1.left, root2.right) && isMirror(root1.right, root2.left);
+      
     }
     return root1 === root2
 
